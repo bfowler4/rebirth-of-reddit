80090816c215d6f694f49c75173c1b10a258d9d5
Brandon Fowler <bmfowler4@gmail.com>
2018-01-17 11:21:49 -1000

    set up fonts

--- public/js/app.js
+++ public/js/app.js
@@ -12,14 +12,21 @@ function buildPage(sub) {
 
 function fetchData(event) {
   let data = JSON.parse(event.target.responseText);
-  console.log(data);
   let keys = [`author`, `title`, `created_utc`, `selftext`, `thumbnail`, `url`];
   let res = [];
 
   data.data.children.forEach((curr, index) => {
     res.push({});
     keys.forEach((key) => {
-      res[index][key] = curr.data[key];
+      if (key === `thumbnail`) {
+        if (curr.data.hasOwnProperty(`preview`)) {
+          res[index][key] = curr.data.preview.images[0].source.url;
+        } else {
+          res[index][key] = ``;
+        }
+      } else {
+        res[index][key] = curr.data[key];
+      }
     });
   })
   return res;
@@ -52,12 +59,14 @@ function buildPost(data) {
     thumbnail.style.backgroundImage = `url('${data.thumbnail}')`;
   }
   container.appendChild(thumbnail);
-  container.appendChild(createElement(`h2`, `post_title`, data.title));
+  let h2_container = createElement(`div`, `h2_container`);
+  h2_container.appendChild(createElement(`h2`, `post_title`, data.title));
+  container.appendChild(h2_container);
   let timeSincePosted = moment.unix(data['created_utc']).utc().fromNow();
-  container.appendChild(
+  h2_container.appendChild(
     createElement(`h3`, `post_details`, `by ${data.author} - ${timeSincePosted}`)
   );
-  container.appendChild(createElement(`p`, `post_text`, data.selftext));
+  h2_container.appendChild(createElement(`p`, `post_text`, data.selftext));
   container.addEventListener(`click`, (event) => {
     window.open(data.url);
   });
