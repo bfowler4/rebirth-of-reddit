b510bd06ba70a44b175ddc4e9f6ee4b07d225b8a
Brandon Fowler <bmfowler4@gmail.com>
2018-01-17 19:05:32 -1000

    finished resizing on small medium and large

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,50 @@
+buildPage(`Dirtybird`);
+document.getElementById(`navigation_bar`).addEventListener(`click`, (event) => {
+  event.preventDefault();
+  switch (event.target.innerHTML) {
+    case `DIRTYBIRD`:
+      buildPage(`Dirtybird`);
+      break;
+    case `CS`:
+      buildPage(`cscareerquestions`);
+      break;
+    case `EAT`:
+      buildPage(`EatCheapAndHealthy`);
+      break;
+    case `RANDOM`:
+      buildPage(`random`);
+      break;
+  }
+});
+document.getElementById(`instagram_grey`).addEventListener(`mouseover`, (event) => {
+  event.target.src = `assets/instagram_orange.svg`;
+});
+document.getElementById(`instagram_grey`).addEventListener(`mouseout`, (event) => {
+  event.target.src = `assets/instagram_grey.svg`;
+});
+document.getElementById(`facebook_grey`).addEventListener(`mouseover`, (event) => {
+  event.target.src = `assets/facebook_orange.svg`;
+});
+document.getElementById(`facebook_grey`).addEventListener(`mouseout`, (event) => {
+  event.target.src = `assets/facebook_grey.svg`;
+});
+
 function buildPage(sub) {
   let xhr = new XMLHttpRequest();
   xhr.addEventListener(`load`, function (event) {
     let data = fetchData(event);
     let container = createElement(`div`, `page_inner_div`);
-    data.forEach((curr) => {
-      container.appendChild(buildPost(curr));
+    let doublePostContainer = createElement(`div`, `double_post_container`);
+    data.forEach((curr, index) => {
+      let post = createPost(curr);
+      if (index % 2 === 0) {
+      } else {
+      }
+      doublePostContainer.appendChild(post);
+      if (index % 2 !== 0) {
+        container.appendChild(doublePostContainer);
+        doublePostContainer = createElement(`div`, `double_post_container`);
+      }
     });
     document.getElementById(`page_container`).innerHTML = ``;
     document.getElementById(`page_container`).innerHTML = container.innerHTML;
@@ -46,7 +86,7 @@ function createElement(type, className, innerHTML) {
   return element;
 }
 
-function buildPost(data) {
+function createPost(data) {
   let container = createElement(`div`, `post_container`);
   let thumbnail = createElement(`div`, `thumbnail`);
   if (!data.thumbnail || data.thumbnail === `self`) {
@@ -55,36 +95,17 @@ function buildPost(data) {
     thumbnail.style.backgroundImage = `url('${data.thumbnail}')`;
   }
   container.appendChild(thumbnail);
-  let h2_container = createElement(`div`, `h2_container`);
-  h2_container.appendChild(createElement(`h2`, `post_title`, data.title));
-  container.appendChild(h2_container);
+  let text_container = createElement(`div`, `text_container`);
+  text_container.appendChild(createElement(`h2`, `post_title`, data.title));
+  container.appendChild(text_container);
   let timeSincePosted = moment.unix(data['created_utc']).utc().fromNow();
   let post_details = createElement(`ul`, `post_details`);
   post_details.appendChild(createElement(`li`, `post_data`, `by ${data.author}`));
   post_details.appendChild(createElement(`li`, `nobullet post_data`, `${timeSincePosted}`));
-  h2_container.appendChild(post_details);
-  h2_container.appendChild(createElement(`p`, `post_text`, data.selftext));
+  text_container.appendChild(post_details);
+  text_container.appendChild(createElement(`p`, `post_text`, data.selftext));
   container.addEventListener(`click`, (event) => {
     window.open(data.url);
   });
-  return container;
+  return container; 
 }
-
-let dirtybird = buildPage(`Dirtybird`);
-document.getElementById(`navigation_bar`).addEventListener(`click`, (event) => {
-  event.preventDefault();
-  switch (event.target.innerHTML) {
-    case `DIRTYBIRD`:
-      buildPage(`Dirtybird`);
-      break;
-    case `CS`:
-      buildPage(`cscareerquestions`);
-      break;
-    case `EAT`:
-      buildPage(`EatCheapAndHealthy`);
-      break;
-    case `RANDOM`:
-      buildPage(`random`);
-      break;
-  }
-});
